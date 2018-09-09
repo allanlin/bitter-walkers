3b639f837eae9e3b8fa5738f9028cb672d099d5d
Allan Lin <allanlin@gmail.com>
2018-09-09 01:45:33 -0400

    hook up to database

--- server.js
+++ server.js
@@ -4,9 +4,28 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: false
+});
+
 // API calls
 app.get('/api/hello', (req, res) => {
   res.send({ express: ' (Server) API call (api/hello).' });
+});
+
+app.get('/api/db', async (req, res) => {
+    try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM test_table');
+
+        res.json(result.rows);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 });
 
 if (process.env.NODE_ENV === 'production') {
